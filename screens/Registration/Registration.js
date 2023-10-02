d9e73eb62eb6ccf62141a32163dac91b23b928f7
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-10-02 10:07:45 +0330

    Update Registration.js

--- screens/Registration/Registration.js
+++ screens/Registration/Registration.js
@@ -1,51 +1,62 @@
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, View, Pressable} from 'react-native';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 import Input from '../../components/Input/Input';
-import style from './style';
-import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import BackButton from '../../components/BackButton/BackButton';
 import {createUser} from '../../api/user';
-const Registration = ({navigation})=> {
-    const [email, setEmail]= useState('');
-    const [password, setPassword]= useState('');
-    console.log(email);
-    return (
-        <SafeAreaView style={[globalStyle.backgroundWhite,globalStyle.flex ]}>
-            <View style={style.backButton}>
-                <BackButton onPress={()=>navigation.goBack() }/>
-            </View>
-                <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={style.container} >
-                    <View style={globalStyle.marginBottom24}>
-                        <Header type={1} title={"Welcome Back"}/>
-                    </View>
-                    <View>
-                        <View style={globalStyle.marginBottom24}>
-                            <Input 
-                                 keyboardType={'email-address'}
-                                label={'Email'} 
-                                placeholder={'Enter your email'} 
-                                onChangeText={(value)=> setEmail(value)} />
-                        </View>
-                        <View style={globalStyle.marginBottom24}>
-                            <Input
-                                secureTextEntry={true}
-                                label={'Password'}
-                                placeholder={'******'}
-                                onChangeText={value => setEmail(value)}/>
-          
-                        </View>
-                        <View>
-                            <Button  title={'Login'}
-                              onPress={async () => await createUser(fullName, email, password)}
-                             />
-                        </View>
-                      
-                    </View>
-                </ScrollView>
-        </SafeAreaView>
-    );
+import style from './style';
+import globalStyle from '../../assets/styles/globalStyle';
+
+const Registration = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <ScrollView>
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <View style={style.backButton}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={style.container}>
+        <View style={globalStyle.marginBottom24}>
+          <Header type={1} title={'Hello and Welcome!'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            label={'First & Last Name'}
+            placeholder={'Enter your full name...'}
+            onChangeText={value => setFullName(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType={'email-address'}
+            label={'Email'}
+            placeholder={'Enter your email...'}
+            onChangeText={value => setEmail(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            secureTextEntry={true}
+            label={'Password'}
+            placeholder={'******'}
+            onChangeText={value => setPassword(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Button
+            title={'Registration'}
+            onPress={async () => await createUser(fullName, email, password)}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+    </ScrollView>
+  );
 };
 
 export default Registration;
